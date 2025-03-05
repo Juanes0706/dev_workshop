b56306e72c4b3b3f2c3a91192c29a48edbf0834d
Juan Esteban Tovar <jetovar50@ucatolica.edu.co>
2025-03-05 15:39:05 -0500

    Se añadio funcion de calcular el area de un poligono regular

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -97,18 +97,7 @@ class Geometria:
        
     
     def area_poligono_regular(self, num_lados, lado, apotema):
-        """
-        Calcula el área de un polígono regular.
-        
-        Args:
-            num_lados (int): Número de lados del polígono
-            lado (float): Longitud de cada lado
-            apotema (float): Longitud de la apotema
-            
-        Returns:
-            float: Área del polígono regular
-        """
-        pass
+       return (num_lados* lado * apotema) / 2
     
     def perimetro_poligono_regular(self, num_lados, lado):
         """
